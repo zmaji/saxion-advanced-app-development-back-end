cb31bcc447e9a0e81adfa3e7efc01eb89f15ca58
zmaji <502241@student.saxion.nl>
2023-09-21 17:47:58 +0200

    wip

--- Assignment-1/Deno/main.ts
+++ Assignment-1/Deno/main.ts
@@ -1,8 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Application();
+
+app.use((ctx, next) => {
+  // called on each request with the context (`ctx`) of the request,
+  // response, and other data.
+  // `next()` is use to modify the flow control of the middleware stack.
+});
+
+app.listen({ port: 8080 });
