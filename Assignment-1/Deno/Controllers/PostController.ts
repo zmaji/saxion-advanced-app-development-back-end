e462127d9e9110256a0c8e7e2f37b7bfdc137651
Maurice <502241@student.saxion.nl>
2023-09-23 12:15:07 +0200

    Post GET joins and referencing

--- Assignment-1/Deno/Controllers/PostController.ts
+++ Assignment-1/Deno/Controllers/PostController.ts
@@ -1,9 +1,34 @@
 import client from "../Database/Connection.ts";
 
+ // @ts-ignore
+const mapRowToPost = (row) => ({
+  id: row.id,
+  userID: row.userID,
+  title: row.title,
+  content: row.content,
+  category: row.category,
+  likes: row.likes,
+  dislikes: row.dislikes,
+  User: {
+    id: row.id,
+    firstName: row.firstName,
+    lastName: row.lastName,
+    email: row.email,
+    nickName: row.nickName,
+    avatar: row.avatar
+  },
+});
+
 const getAllPosts = async () => {
   try {
-    const result = await client.query("SELECT * FROM posts");
-    return result;
+    const results = await client.query(`
+      SELECT * FROM posts
+      INNER JOIN users
+      ON posts.userID = users.id
+    `);
+
+    const posts = results.map(mapRowToPost);
+    return posts;
   } catch (error) {
     console.error('Error retrieving posts:', error);
     throw error;
@@ -12,8 +37,16 @@ const getAllPosts = async () => {
 
 const getPostById = async (postId: string) => {
   try {
-    const result = await client.query("SELECT * FROM posts WHERE id = ?", [postId]);
-    return result;
+    const query = `
+      SELECT * FROM posts
+      INNER JOIN users ON
+        posts.userID = users.id
+      WHERE
+        posts.id = ?`;
+        
+    const result = await client.query(query, [postId]);
+    const post = mapRowToPost(result[0]);
+    return post;
   } catch (error) {
     console.error(`Error retrieving post with ID ${postId}:`, error);
     throw error;
