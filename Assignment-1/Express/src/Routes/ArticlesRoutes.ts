e3be0af1ce81f96e861e3649065bacb21357fbd5
Maurice <502241@student.saxion.nl>
2023-10-02 14:50:02 +0200

    added guards to all routes

--- Assignment-1/Express/src/Routes/ArticlesRoutes.ts
+++ Assignment-1/Express/src/Routes/ArticlesRoutes.ts
@@ -1,89 +1,91 @@
 import { Router, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import ArticleController from '../Controllers/ArticleController';
+import isAdmin from '../Middleware/is-admin';
+import isLoggedIn from '../Middleware/is-logged-in';
 
 const router = Router();
 
 router.get('', async (req: Request, res: Response) => {
-    try {
-        const result = await ArticleController.getArticles();
-        res
-            .status(StatusCodes.OK)
-            .send(result);
-    } catch (error) {
-        res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: 'An error occurred' });
-    }
+  try {
+    const result = await ArticleController.getArticles();
+    res
+      .status(StatusCodes.OK)
+      .send(result);
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'An error occurred' });
+  }
 });
 
 router.get('/:articleID', async (req: Request, res: Response) => {
-    try {
-        const result = await ArticleController.getArticle(req.params.articleID);
-        if (result) {
-            res
-                .status(StatusCodes.OK)
-                .send(result);
-        } else {
-            res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ error: 'Unable to find article' });
-        }
-    } catch (error) {
-        res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: 'An error occurred' });
+  try {
+    const result = await ArticleController.getArticle(req.params.articleID);
+    if (result) {
+      res
+        .status(StatusCodes.OK)
+        .send(result);
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Unable to find article' });
     }
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'An error occurred' });
+  }
 });
 
-router.post('', async (req: Request, res: Response) => {
-    try {
-        const article = await ArticleController.createArticle(req.body);
-        res
-            .status(StatusCodes.CREATED)
-            .json(article);
-    } catch (error) {
-        res
-            .status(StatusCodes.BAD_REQUEST)
-            .json({ error: 'Please make sure to enter all fields correctly' });
-    }
+router.post('', isLoggedIn, isAdmin, async (req: Request, res: Response) => {
+  try {
+    const article = await ArticleController.createArticle(req.body);
+    res
+      .status(StatusCodes.CREATED)
+      .json(article);
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: 'Please make sure to enter all fields correctly' });
+  }
 });
 
-router.put('/:articleID', async (req: Request, res: Response) => {
-    try {
-        const updatedArticle = await ArticleController.updateArticle(req.params.articleID, req.body);
-        if (updatedArticle) {
-            res
-                .status(StatusCodes.OK)
-                .json(updatedArticle);
-        } else {
-            res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ error: 'Unable to find article' });
-        }
-    } catch (error) {
-        res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: 'An error occurred' });
+router.put('/:articleID', isLoggedIn, isAdmin, async (req: Request, res: Response) => {
+  try {
+    const updatedArticle = await ArticleController.updateArticle(req.params.articleID, req.body);
+    if (updatedArticle) {
+      res
+        .status(StatusCodes.OK)
+        .json(updatedArticle);
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Unable to find article' });
     }
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'An error occurred' });
+  }
 });
 
-router.delete('/:articleID', async (req: Request, res: Response) => {
-    try {
-        const result = await ArticleController.deleteArticle(req.params.articleID);
-        if (result) {
-            res
-                .sendStatus(StatusCodes.NO_CONTENT)
-        } else {
-            res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ error: 'Unable to find article' });
-        }
-    } catch (error) {
-        res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: 'An error occurred' });
+router.delete('/:articleID', isLoggedIn, isAdmin, async (req: Request, res: Response) => {
+  try {
+    const result = await ArticleController.deleteArticle(req.params.articleID);
+    if (result) {
+      res
+        .sendStatus(StatusCodes.NO_CONTENT)
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Unable to find article' });
     }
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'An error occurred' });
+  }
 });
 
 export default router;
