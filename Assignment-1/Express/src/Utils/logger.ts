02791f16a9e234996199204c29fe52a8afb18d18
Maurice <502241@student.saxion.nl>
2023-11-03 11:36:23 +0100

    trialing mistakes, reversed docker entry and ran linter

--- Assignment-1/Express/src/Utils/logger.ts
+++ Assignment-1/Express/src/Utils/logger.ts
@@ -30,22 +30,22 @@ const logFormat = printf(({ timestamp, level, message, stack }) => {
 const logger = createLogger({
   levels: customLevels.levels,
   format: combine(
-    errors({ stack: true }),
-    timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-    logFormat,
+      errors({ stack: true }),
+      timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+      logFormat,
   ),
   transports: [
     new transports.Console({
       format: combine(
-        colorize(),
-        logFormat,
+          colorize(),
+          logFormat,
       ),
     }),
     new transports.File({
       filename: './Logs/application.log',
       format: combine(
-        prettyPrint(),
-        logFormat,
+          prettyPrint(),
+          logFormat,
       ),
       options: { flags: 'a' },
     }),
